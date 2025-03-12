6e54b105e13f19d9d252bd2bd92e68d3533b54c7
Manap <121767281+manap01@users.noreply.github.com>
2025-03-13 00:57:01 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="deteksi_objek",  # Nama package
+    version="1.0.0",
+    author="Hanif Maulana Arrasyid",
+    description="Package untuk deteksi objek menggunakan machine learning",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "opencv-python",
+        "torch",
+        "tensorflow"
+    ],
+)
